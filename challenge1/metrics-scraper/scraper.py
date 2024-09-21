974e836177651a2619fdc46c26113f8b2b2fff11
Noorghori <116746109+Noorghori@users.noreply.github.com>
2024-09-21 19:42:40 +0530

    Add files via upload

---
+++ challenge1/metrics-scraper/scraper.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+import psutil
+
+app = Flask(__name__)
+
+@app.route('/metrics')
+def metrics():
+    cpu = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory().percent
+    return jsonify(cpu=cpu, memory=memory)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
